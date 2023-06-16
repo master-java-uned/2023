c7b22e8d7794c15f6360cbda8d64bc391177990c
Dinora15 <108556884+Dinora15@users.noreply.github.com>
2023-06-15 18:27:10 -0600

    Update Conector.java

--- healthmap/src/main/java/unedMasterJavaModelo/Conector.java
+++ healthmap/src/main/java/unedMasterJavaModelo/Conector.java
@@ -1,1 +1,27 @@
+package unedMasterJavaModelo;
+import java.sql.*;
+
+public class Conector {
+
+	private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection != null)
+            return connection;
+        else {
+            try {
+                
+                Class.forName("com.mysql.cj.jdbc.Driver");
+               
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/health_map", "root", "");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
+
+    }
+}
 
